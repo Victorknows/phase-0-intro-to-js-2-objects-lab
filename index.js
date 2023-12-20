bcaaee89967646aeaf5ffdcf0e35031820d8b0f8
Victor Muhoro <victormuhoro7@gmail.com>
2023-12-20 08:53:55 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution in this file!
+const employee = {
+    name :"Victor",
+    streetAddress : "Lumumba drive",
+}
+
+function updateEmployeeWithKeyAndValue(){
+    const employee_1 = {
+        name : "Sam",
+        streetAddress : "11 Broadway"
+    }
+    const updatedEmployees = {...employee, ...employee_1}
+    return updatedEmployees;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue() {
+    employee.name = "Sam";
+    employee.streetAddress = "12 Broadway";
+    return employee;
+}
+
+function deleteFromEmployeeByKey() {
+    const employee_2 = {
+        color : "black",
+        road : "trm",
+    }
+    const about = {...employee, ...employee_2}
+    return about;
+}
+
+
+
+
+
+
+
+
